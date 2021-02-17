f47d91624f77d32adad9af8d1480d8c85bf1990f
조상호 <97chos@naver.com>
2021-02-17 13:09:11 +0900

    whetherDetailViewController 생성

---
+++ weatherToday/weatherToday/whether/weatherDetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  weatherDetailViewController.swift
+//  weatherToday
+//
+//  Created by sangho Cho on 2021/02/17.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class whetherDetailViewController: UIViewController {
+
+  // MARK: Properties
+
+  private let city: City!
+
+  // MARK: UI
+
+
+  // MARK: Initializing
+
+  init(city: City) {
+    self.city = city
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: View LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.configure()
+  }
+
+
+  // MARK: Congigure
+
+  private func configure() {
+    self.viewConfigure()
+  }
+
+  private func viewConfigure() {
+    self.view.backgroundColor = .systemBackground
+  }
+}
