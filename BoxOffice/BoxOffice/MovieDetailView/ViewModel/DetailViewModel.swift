8c120993a0be50c0f45f1d1382ce663057740239
조상호 <97chos@naver.com>
2021-03-15 23:58:29 +0900

    DetailViewModel 분리

---
+++ BoxOffice/BoxOffice/MovieDetailView/ViewModel/DetailViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewModel.swift
+//  BoxOffice
+//
+//  Created by sangho Cho on 2021/03/14.
+//
+
+import Foundation
+
+class DetailViewModel {
+
+  // MARK: Properties
+
+  let movie: Movie
+
+
+  // MARK: Initializing
+
+  init(movie: Movie) {
+    self.movie = movie
+  }
+}
