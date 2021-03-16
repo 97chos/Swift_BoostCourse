ad8ce5c2ee71ddaebef326bab0de0db6c5d2e1d2
조상호 <97chos@naver.com>
2021-03-16 16:14:21 +0900

    영화 정보 및 영화 리뷰 데이터를 가져오는 API 메소드 추가

--- BoxOffice/BoxOffice/MovieDetailView/ViewModel/DetailViewModel.swift
+++ BoxOffice/BoxOffice/MovieDetailView/ViewModel/DetailViewModel.swift
@@ -7,16 +7,93 @@
 
 import Foundation
 
+protocol layoutUpdateDelegate: class {
+  func layoutReload()
+}
+
+enum DetailViewSections {
+  case movieInformation
+  case synopsis
+  case crew
+  case review
+}
+
+
 class DetailViewModel {
+
 
   // MARK: Properties
 
-  let movie: Movie
+  let sectionList: [DetailViewSections] = [.movieInformation,.synopsis,.crew,.review]
+  var movie: [MovieInfoModel] = []
+  var moviewReviews: [MoviewReviewModel] = []
+
+  let loadMovieUrlString = "connect-boxoffice.run.goorm.io/movie?"
+  let loadReviewUrlString = "connect-boxoffice.run.goorm.io/comments?"
+  weak var delegate: layoutUpdateDelegate?
 
 
   // MARK: Initializing
 
-  init(movie: Movie) {
-    self.movie = movie
+  init(movieId: String) {
+    self.setData(id: movieId)
+    self.loadReviews(id: movieId)
   }
+
+
+  // MARK: Functions
+
+  func setData(id: String) {
+    var components = URLComponents(string: self.loadMovieUrlString)
+    components?.scheme = "http"
+    components?.queryItems?.append(URLQueryItem(name: "id", value: id))
+    guard let componentsURL = components?.url else { return }
+
+    let request: URLRequest = URLRequest(url: componentsURL)
+
+    URLSession(configuration: .default).dataTask(with: request) { data, response, error in
+      guard error == nil else {
+        return
+      }
+      guard let resultData = data else {
+        return
+      }
+
+      do {
+        let result = try JSONDecoder().decode(MovieInfoModel.self, from: resultData)
+        self.movie = [result]
+      } catch {
+        return
+      }
+    }.resume()
+  }
+
+  func loadReviews(id: String) {
+    var components = URLComponents(string: self.loadReviewUrlString)
+    components?.scheme = "http"
+    components?.queryItems?.append(URLQueryItem(name: "id", value: id))
+    guard let componentsURL = components?.url else { return }
+
+    let request: URLRequest = URLRequest(url: componentsURL)
+
+    URLSession(configuration: .default).dataTask(with: request) { data, response, error in
+      guard error == nil else {
+        return
+      }
+      guard let resultData = data else {
+        return
+      }
+
+      do {
+        let result = try JSONDecoder().decode([MoviewReviewModel].self, from: resultData)
+        self.moviewReviews = result
+        DispatchQueue.main.async {
+          self.delegate?.layoutReload()
+        }
+      } catch {
+        return
+      }
+    }.resume()
+  }
+  
 }
