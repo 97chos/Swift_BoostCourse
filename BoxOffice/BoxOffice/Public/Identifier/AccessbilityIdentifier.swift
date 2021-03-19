98e6632588fa681d6a66ad3b5671ed301e346612
조상호 <97chos@naver.com>
2021-03-19 12:30:07 +0900

    이미지 AccessbilityIdentifier 사용을 위한 enum 추가

---
+++ BoxOffice/BoxOffice/Public/Identifier/AccessbilityIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  AccessbilityIdentifier.swift
+//  BoxOffice
+//
+//  Created by sangho Cho on 2021/03/19.
+//
+
+import Foundation
+
+enum AccessbilityIdentifier {
+  static let imagePlaceholderIdentifier = "placeholderImage"
+}
---
+++ BoxOffice/BoxOffice/Public/Identifier/ReuseIdentifier.swift
@@ -0,0 +1,19 @@
+//
+//  CellReuseIdentifier.swift
+//  BoxOffice
+//
+//  Created by sangho Cho on 2021/03/19.
+//
+
+import Foundation
+
+enum reuseIdentifier {
+  static let reuseTableViewCell = "ReusableTableViewCell"
+  static let reuseCollectionViewCell = "ReusableCollectionViewCell"
+  static let reuseDetailViewInforCell = "ReuseDetailViewInforCell"
+  static let reuseDetailViewsynopsisCell = "ReuseDetailViewSynopsisCell"
+  static let reuseDetailViewCrewCell = "ReuseDetailViewCrewCell"
+  static let reuseDetailViewReviewHeaderCell = "ReuseDetailViewReviewHeaderCell"
+  static let reuseDetailViewReviewCell = "ReuseDetailViewReviewCell"
+}
+
