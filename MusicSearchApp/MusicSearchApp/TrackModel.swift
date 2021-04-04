4f421bf4bf3bbc4ee774eab4bb9e98ff495b15fb
조상호 <97chos@naver.com>
2021-04-05 02:15:28 +0900

    indent 수정

--- MusicSearchApp/MusicSearchApp/TrackModel.swift
+++ MusicSearchApp/MusicSearchApp/TrackModel.swift
@@ -10,20 +10,21 @@ import UIKit
 
 
 struct Response: Codable {
-    let resultCount: Int
-    let results: [Track]
+  let resultCount: Int
+  let results: [Track]
 }
 
 struct Track: Codable {
-    var title: String
-    var artistName: String
-    var thumbnail: String
-    let previewUrl: String?
+  var title: String
+  var artistName: String
+  var thumbnail: String
+  let previewUrl: String?
 
-    enum CodingKeys: String, CodingKey {
-        case title = "trackName"
-        case artistName = "artistName"
-        case thumbnail = "artworkUrl30"
-        case previewUrl = "previewUrl"
-    }
+
+  enum CodingKeys: String, CodingKey {
+    case title = "trackName"
+    case artistName = "artistName"
+    case thumbnail = "artworkUrl30"
+    case previewUrl = "previewUrl"
+  }
 }
