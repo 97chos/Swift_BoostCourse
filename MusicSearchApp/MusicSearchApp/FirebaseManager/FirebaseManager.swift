250d4cc2e44a93ce84c0147985f8a5b7ec821a80
조상호 <97chos@naver.com>
2021-04-05 00:52:13 +0900

    Firebase 서버에서 데이터를 저장 및 불러올 수 있는 Manager 파일 추가

---
+++ MusicSearchApp/MusicSearchApp/FirebaseManager/FirebaseManager.swift
@@ -0,0 +1,75 @@
+//
+//  FirebaseManager.swift
+//  MusicSearchApp
+//
+//  Created by sangho Cho on 2021/04/05.
+//
+
+import Foundation
+import Firebase
+
+enum FirebaseKey {
+  static let search = "searchHistory"
+  static let watch = "watchHistory"
+}
+
+class FirebaseManager {
+
+  // MARK: Properties
+
+  static let shared: FirebaseManager = FirebaseManager()
+  let rootRef = Database.database().reference()
+  var searchHistory: [String] = []
+  var watchHistory: [Track] = []
+
+
+  // MARK: Initializing
+
+  private init() {}
+
+
+  // MARK: Send Data To Server
+
+  func addSearchHistory(keyword: String) {
+    self.searchHistory.insert(keyword, at: 0)
+    self.rootRef.child(FirebaseKey.search).setValue(self.searchHistory)
+  }
+
+  func addWatchHistory(track: Track) {
+    self.watchHistory.insert(track, at: 0)
+    let tracks: [[String: Any]] = self.watchHistory.map{ $0.toDictionary }
+    self.rootRef.child(FirebaseKey.watch).setValue(tracks)
+  }
+
+
+  // MARK: Get Data From Server
+
+  func fetchSearchHistory(completion: @escaping () -> Void) {
+    self.rootRef.child(FirebaseKey.search).queryLimited(toLast: 3).observeSingleEvent(of: .value) { snapShot in
+      guard let searches = snapShot.value as? [String] else { return }
+      self.searchHistory = searches
+
+      DispatchQueue.main.async {
+        completion()
+      }
+    }
+  }
+
+  func fetchWatchHistory(completion: @escaping () -> Void) {
+    self.rootRef.child(FirebaseKey.watch).queryLimited(toLast: 3).observeSingleEvent(of: .value) { snapShot in
+      guard let watches = snapShot.value as? [[String : Any]] else { return }
+      do {
+        let data = try JSONSerialization.data(withJSONObject: watches, options: [])
+        let tracks = try JSONDecoder().decode([Track].self, from: data)
+        self.watchHistory = tracks
+
+        DispatchQueue.main.async {
+          completion()
+        }
+      } catch {
+        print(error.localizedDescription)
+      }
+    }
+  }
+
+}
