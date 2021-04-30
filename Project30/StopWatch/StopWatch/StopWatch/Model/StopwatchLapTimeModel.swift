a6c219c32dae9150317c9d27d6e9e20c009c4ad8
조상호 <97chos@naver.com>
2021-04-30 13:12:57 +0900

    lapTime과 mainTime을 기록하기 위한 StopwatchTimeModel 생성

---
+++ Project30/StopWatch/StopWatch/StopWatch/Model/StopwatchLapTimeModel.swift
@@ -0,0 +1,13 @@
+//
+//  StopwatchLapTimeModel.swift
+//  StopWatch
+//
+//  Created by sangho Cho on 2021/04/30.
+//
+
+import Foundation
+
+struct StopwatchTimeModel {
+  let lapTime: String
+  let mainTime: String
+}
