6a23c6a1437031702eb1b42f410b82a900f08abb
조상호 <97chos@naver.com>
2021-05-01 23:55:18 +0900

    UI 추가

--- Project30/CandySearch/CandySearch/ViewController.swift
+++ Project30/CandySearch/CandySearch/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  // MARK: UI
+
+  let tableView: UITableView = {
+    let tableView = UITableView()
+    return tableView
+  }()
+  let searchController: UISearchController = {
+    let searchController = UISearchController(searchResultsController: nil)
+    searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
+    searchController.obscuresBackgroundDuringPresentation = true
+    return searchController
+  }()
+
+
+  // MARK: View LifeCycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
