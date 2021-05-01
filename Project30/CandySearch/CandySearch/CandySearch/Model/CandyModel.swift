66979244099af476fa0f7ef07fabfbe59fe3df6f
조상호 <97chos@naver.com>
2021-05-01 23:59:44 +0900

    Candy 모델 생성

---
+++ Project30/CandySearch/CandySearch/CandySearch/Model/CandyModel.swift
@@ -0,0 +1,14 @@
+//
+//  CandyModel.swift
+//  CandySearch
+//
+//  Created by sangho Cho on 2021/05/01.
+//
+
+import Foundation
+
+
+struct CandyModel {
+  let type: String
+  let category: String
+}
