342618e0a30a329e7230f4b2a2a9ad535dacb2a1
조상호 <97chos@naver.com>
2021-05-02 12:11:46 +0900

    ViewModel 생성

---
+++ Project30/CandySearch/CandySearch/CandySearch/ViewModel/SearchViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewModel.swift
+//  CandySearch
+//
+//  Created by sangho Cho on 2021/05/02.
+//
+
+import Foundation
+
+class SearchViewModel {
+
+  // MARK: Properties
+
+  private let candies: [Candy] = [
+    Candy(type: .chocolate, name: "Chocolate Bar"),
+    Candy(type: .chocolate, name: "Chocolate Chip"),
+    Candy(type: .chocolate, name: "Dark Chocolate"),
+    Candy(type: .hard, name: "Lollipop"),
+    Candy(type: .hard, name: "Jaw Breaker"),
+    Candy(type: .other, name: "Caramel"),
+    Candy(type: .other, name: "Sour Chew"),
+    Candy(type: .other, name: "Gummi Bear")
+  ]
+
+  lazy var filteredCandies: [Candy] = self.candies
+
+
+  // MARK: Functions
+
+  func filterContent(_ text: String, _ scope: String = "All") {
+    self.filteredCandies = candies.filter {
+      if $0.type.rawValue != scope && scope != "All" {
+        return false
+      }
+
+      return $0.name.lowercased().contains(text.lowercased()) || text == ""
+    }
+
+  }
+  
+}
