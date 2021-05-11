36af99582fffb2c6a61694ebb51cbd5fab636ff3
조상호 <97chos@naver.com>
2021-05-12 00:30:43 +0900

    초기 sceneDelegate 설정

--- Project30/SimpleRSSReader/SimpleRSSReader/SceneDelegate.swift
+++ Project30/SimpleRSSReader/SimpleRSSReader/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let scene = (scene as? UIWindowScene) else { return }
 
-    let navigationController = UINavigationController(rootViewController: ViewController())
+    let navigationController = UINavigationController(rootViewController: ViewController(viewModel: ViewModel()))
     self.window = UIWindow(frame: UIScreen.main.bounds)
     self.window?.rootViewController = navigationController
     self.window?.windowScene = scene
