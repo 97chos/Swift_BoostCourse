34b7d382b12acb3fd7d2870a39940adc9ceee988
조상호 <97chos@naver.com>
2021-05-13 01:30:53 +0900

    collectionViewCell 생성

--- Project30/Interest/Interest/InterestCollectionViewCell.swift
+++ Project30/Interest/Interest/InterestCollectionViewCell.swift
@@ -6,8 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+
+  // MARK: UI
+
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    return imageView
+  }()
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
