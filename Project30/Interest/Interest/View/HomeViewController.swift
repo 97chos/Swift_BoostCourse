ee5082aa05f7720e27da318537b3561fb6ce2c52
조상호 <97chos@naver.com>
2021-05-15 22:03:14 +0900

    HomeViewController Initailizing 업데이트

--- Project30/Interest/Interest/View/HomeViewController.swift
+++ Project30/Interest/Interest/View/HomeViewController.swift
@@ -32,6 +32,23 @@ class HomeViewController: UIViewController {
   }()
 
 
+  // MARK: Properties
+
+  private var viewModel: HomeViewModel
+
+
+  // MARK: Initailizing
+
+  init(viewModel: HomeViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+
   // MARK: View LifeCycle
 
   override func viewDidLoad() {
