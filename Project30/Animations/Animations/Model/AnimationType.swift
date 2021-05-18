1f9ee6749c74b6cff39ad7653c1daba36c34b90d
조상호 <97chos@naver.com>
2021-05-18 16:19:39 +0900

    AnimationType 구현

--- Project30/Animations/Animations/Model/AnimationType.swift
+++ Project30/Animations/Animations/Model/AnimationType.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+enum AnimationType: String, CaseIterable {
+  case twoColor = "2-Color"
+  case simple2DRotation = "Simple 2D Rotation"
+  case multiColor = "MultiColor"
+  case multiPointPosition = "Multi Point Position"
+  case bezrierCurvePosition = "BezierCurve Position"
+  case colorAndFrameChange = "Color and Frame Change"
+  case viewFadeIn = "View Fade In"
+  case pop = "Pop"
+}
+
