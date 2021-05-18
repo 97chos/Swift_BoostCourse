2c95da68f89cffd6a9065b21bbd61c1a0203880a
조상호 <97chos@naver.com>
2021-05-18 16:16:24 +0900

    UI 추가

--- Project30/Animations/Animations/ViewController.swift
+++ Project30/Animations/Animations/ViewController.swift
@@ -1,13 +1,29 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  Animations
 //
 //  Created by sangho Cho on 2021/05/18.
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
+
+  // MARK: UI
+
+  private let tableView: UITableView = {
+    let tableView = UITableView()
+    return tableView
+  }()
+
+
+  // MARK: Properties
+
+  private let items: [AnimationType] = AnimationType.allCases
+
+
+  // MARK: View Life Cycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
