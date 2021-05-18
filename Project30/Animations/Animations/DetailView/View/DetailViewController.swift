646c56934f03f78b57cdc6d56bbb3113aea08611
조상호 <97chos@naver.com>
2021-05-18 16:28:28 +0900

    Animate Button 기능 구현

--- Project30/Animations/Animations/DetailView/View/DetailViewController.swift
+++ Project30/Animations/Animations/DetailView/View/DetailViewController.swift
@@ -108,6 +108,145 @@ class DetailViewController: UIViewController {
   }
 
 
+  // MARK: Animate Button
+
+  @objc private func changeAnimate(_ sender: UIButton) {
+    switch self.animateType {
+    case .twoColor:
+      self.changeColor(.green)
+
+    case .simple2DRotation:
+      self.rotateView(Double.pi)
+
+    case .multiColor:
+      self.multiColor(.green, .blue)
+
+    case .multiPointPosition:
+      self.multiPosition(CGPoint(x: self.animateView.frame.origin.x, y: 100), CGPoint(x: self.animateView.frame.origin.x, y: 350))
+
+    case .bezrierCurvePosition:
+      var controlPoint1 = self.animateView.center
+      controlPoint1.y -= 125.0
+      var controlPoint2 = controlPoint1
+      controlPoint2.x += 40.0
+      controlPoint2.y -= 125.0
+
+      var endPoint = self.animateView.center
+      endPoint.x += 75.0
+      self.curvePath(endPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+
+    case .colorAndFrameChange:
+      let currentFrame = self.animateView.frame
+      let firstFrame = currentFrame.insetBy(dx: -30, dy: -50)
+      let secondFrame = firstFrame.insetBy(dx: 10, dy: 15)
+      let thirdFrame = secondFrame.insetBy(dx: -15, dy: -20)
+      self.colorFrameChange(firstFrame, secondFrame, thirdFrame, .orange, .yellow, .green)
+
+    case .viewFadeIn:
+      self.viewFadeIn()
+
+    case .pop:
+      self.pop()
+
+    default:
+      break
+    }
+  }
+
+
+  // MARK: Do Animate
+
+  private func changeColor(_ color: UIColor) {
+    UIView.animate(withDuration: self.duration) {
+      self.animateView.backgroundColor = color
+    }
+  }
+
+  private func multiColor(_ firstColor: UIColor, _ secondColor: UIColor) {
+    UIView.animate(withDuration: self.duration, animations: {
+      self.animateView.backgroundColor = firstColor
+    }) { _ in
+      self.changeColor(secondColor)
+    }
+  }
+
+  private func multiPosition(_ firstPos: CGPoint, _ secondPos: CGPoint) {
+    func simplePosition(_ pos: CGPoint) {
+      UIView.animate(withDuration: self.duration) {
+        self.animateView.frame.origin = pos
+      }
+    }
+
+    UIView.animate(withDuration: self.duration, animations: {
+      self.animateView.frame.origin = firstPos
+    }) { _ in
+      simplePosition(secondPos)
+    }
+  }
+
+  private func rotateView(_ angle: Double) {
+    UIView.animate(withDuration: self.duration, delay: self.delay, options: [.repeat], animations: {
+      self.animateView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+    })
+  }
+
+  private func colorFrameChange(_ firstFrame: CGRect, _ secondFrame: CGRect, _ thirdFrame: CGRect, _ firstColor: UIColor, _ secondColor: UIColor, _ thirdColor: UIColor) {
+    UIView.animate(withDuration: self.duration, animations: {
+      self.animateView.backgroundColor = firstColor
+      self.animateView.frame = firstFrame
+    }) { _ in
+      UIView.animate(withDuration: self.duration, animations: {
+        self.animateView.backgroundColor = secondColor
+        self.animateView.frame = secondFrame
+      }) { _ in
+        UIView.animate(withDuration: self.duration, animations: {
+          self.animateView.backgroundColor = thirdColor
+          self.animateView.frame = thirdFrame
+        })
+      }
+    }
+  }
+
+  private func curvePath(_ endPoint: CGPoint, controlPoint1: CGPoint, controlPoint2: CGPoint) {
+    let path = UIBezierPath()
+    path.move(to: self.animateView.center)
+
+    path.addCurve(to: endPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+
+    let anim = CAKeyframeAnimation(keyPath: "position")
+
+    anim.path = path.cgPath
+
+    anim.duration = self.duration
+
+    self.animateView.layer.add(anim, forKey: "animate position along path")
+    self.animateView.center = endPoint
+  }
+
+  private func viewFadeIn() {
+    let secondView = UIImageView(image: UIImage(named: "facebook"))
+    secondView.frame = self.animateView.frame
+    secondView.alpha = 0.0
+
+    self.view.insertSubview(secondView, aboveSubview: self.animateView)
+
+    UIView.animate(withDuration: self.duration, delay: self.delay, options: .curveEaseOut) {
+      secondView.alpha = 1.0
+      self.animateView.alpha = 0.0
+    }
+  }
+
+  private func pop() {
+    UIView.animate(withDuration: duration / 4, animations: {
+      self.animateView.transform = CGAffineTransform(scaleX: CGFloat(self.scale), y: CGFloat(self.scale))
+    }) { _ in
+      UIView.animate(withDuration: self.duration / 4, animations: {
+        self.animateView.transform = CGAffineTransform.identity
+      })
+    }
+  }
+
+
   // MARK: Layout
 
   private func layout() {
