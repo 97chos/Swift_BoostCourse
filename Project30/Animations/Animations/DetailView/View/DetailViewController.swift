1a01d8f12e2a6ab5f21c6eec7a32b3ee41f1092d
조상호 <97chos@naver.com>
2021-05-18 16:10:18 +0900

    resource 및 초기 scene 설정

---
+++ Project30/Animations/Animations/DetailView/View/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  Animations
+//
+//  Created by sangho Cho on 2021/05/18.
+//
+
+import Foundation
---
+++ Project30/Animations/Animations/Model/AnimationType.swift
@@ -0,0 +1,8 @@
+//
+//  AnimationType.swift
+//  Animations
+//
+//  Created by sangho Cho on 2021/05/18.
+//
+
+import Foundation
