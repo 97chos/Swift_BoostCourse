4625a68e75b9a9f5d8fbd236bc7e34fd5e8eeea0
조상호 <97chos@naver.com>
2021-05-19 23:39:33 +0900

    초기 ViewController 구현

--- Project30/Tumblr/Tumblr/ViewController.swift
+++ Project30/Tumblr/Tumblr/ViewController.swift
@@ -5,15 +5,65 @@
 //  Created by sangho Cho on 2021/05/19.
 //
 
-import UIKit
+import Foundation
+import SnapKit
 
 class ViewController: UIViewController {
 
+  // MARK: UI
+
+  private let toolbar: UIToolbar = {
+    let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    return toolbar
+  }()
+  private let searchButton: UIBarButtonItem = {
+    let button = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: nil)
+    return button
+  }()
+  private lazy var showMenuButton: UIBarButtonItem = {
+    let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.showMenu))
+    return button
+  }()
+  private let refreshButton: UIBarButtonItem = {
+    let button = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
+    return button
+  }()
+
+
+  // MARK: View LifeCycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    self.configuration()
+    self.layout()
   }
 
 
-}
+  // MARK: Configuraion
 
+  private func configuration() {
+    self.configureView()
+  }
+
+  private func configureView() {
+    self.view.backgroundColor = .systemBackground
+  }
+
+
+  // MARK: Functions
+
+  @objc private func showMenu() {
+    let menuVC = MenuViewController()
+    self.present(menuVC, animated: true)
+  }
+
+  private func layout() {
+    self.toolbar.setItems([self.searchButton, UIBarButtonItem(systemItem: .flexibleSpace) ,self.showMenuButton, UIBarButtonItem(systemItem: .flexibleSpace), self.refreshButton], animated: true)
+
+    self.view.addSubview(self.toolbar)
+
+    self.toolbar.snp.makeConstraints{
+      $0.bottom.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+    }
+  }
+}
